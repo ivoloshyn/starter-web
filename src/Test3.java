97e544e83394684641bab8b3ecc27a53e9c7c96d
Ihor Voloshyn <xvoligo@training.com>
2020-06-17 15:19:32 +0300

    master 1 in test3 file

--- src/Test3.java
+++ src/Test3.java
@@ -10,5 +10,6 @@ public class Test3 {
         WebDriver driver = new InternetExplorerDriver();
         driver.get("https://google.com");
         System.out.println(driver.getTitle());
+        System.out.println("master 1 in test3 file");
     }
 }
