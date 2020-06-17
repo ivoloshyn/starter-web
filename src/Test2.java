ad4dd3081928432eea035340ff5ed50d62a177e8
Ihor Voloshyn <xvoligo@training.com>
2020-06-17 23:32:34 +0300

    del readln

--- src/Test2.java
+++ src/Test2.java
@@ -9,8 +9,5 @@ public class Test2 {
         System.setProperty("webdriver.gecko.driver", "D:\\Study\\Selenium+java\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
         driver.get("https://google.com.ua");
-        System.out.println(driver.getTitle());
-        System.out.println("from rep");
-        System.out.println("from rep 2");
     }
 }
