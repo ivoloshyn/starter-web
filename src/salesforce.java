0b262202eb2708a0460da59728124e94af594b7a
Ihor Voloshyn <xvoligo@training.com>
2020-06-23 00:46:26 +0300

    31. find be id, xPath

--- src/salesforce.java
+++ src/salesforce.java
@@ -1,14 +1,18 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Test2 {
+public class salesforce {
     public static void main(String[] args) {
         //Create Driver object for firefox browser
         //invoke .exe file first
         //gecko driver
         System.setProperty("webdriver.gecko.driver", "D:\\Study\\Selenium+java\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
-        driver.get("https://google.com.ua");
+        driver.get("https://login.salesforce.com");
+        driver.findElement(By.id("username")).sendKeys("user");
+        driver.findElement(By.xpath("//*[@id='Login']")).click();
+
         // xPath //*[@id="username"] //*[@id="password"] //*[@id="Login"]
     }
 }
