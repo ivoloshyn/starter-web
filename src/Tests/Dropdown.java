fa0de9834941eee554837989d0e4a884642b0baa
Ihor Voloshyn <xvoligo@training.com>
2020-07-07 19:36:14 +0300

    Course 50, final

--- src/Tests/Dropdown.java
+++ src/Tests/Dropdown.java
@@ -1,12 +1,20 @@
 package Tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class Dropdown {
     public static void main(String[] args) {
         System.setProperty("webdriver.gecko.driver", "D:\\Study\\Selenium+java\\geckodriver.exe");
         WebDriver driver = new FirefoxDriver();
-        driver.get("");
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        //driver.findElement(By.id("adultDropdown")).click();
+        Select s= new Select(driver.findElement(By.id("ctl00_mainContent_DropDownListCurrency")));
+        s.selectByValue("AED");
+        s.selectByIndex(2);
+        s.selectByVisibleText("Select");
+        driver.close(); // closes current browser*/
     }
 }
