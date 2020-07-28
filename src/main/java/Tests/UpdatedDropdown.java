9d15ed2b6ffafc1ab6ab46c571e8309de89036bf
Ihor Voloshyn <xvoligo@training.com>
2020-07-28 19:57:05 +0300

    1) files moved to appropriate folders 2) wait test is added

--- src/main/java/Tests/UpdatedDropdown.java
+++ src/main/java/Tests/UpdatedDropdown.java
@@ -1,8 +1,9 @@
-package Tests;
+package main.java.Tests;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 
 public class UpdatedDropdown {
     public static void main(String[] args) throws InterruptedException {
@@ -25,6 +26,20 @@ public class UpdatedDropdown {
         driver.findElement(By.id("btnclosepaxoption")).click();
 
        System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+
+        //  //a[@value='MAA']  - Xpath for chennai
+
+        //  //a[@value='BLR']
+
+        driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+        driver.findElement(By.xpath("//a[@value='BLR']")).click();
+        Thread.sleep(2000);
+        //driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+        driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
+
+        //Assert.assertFalse();
+        //Assert.assertTrue();
+        //Assert.assertEquals();
 
         driver.close(); // closes current browser
     }
